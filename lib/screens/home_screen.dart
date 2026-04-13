41c73e264606cc45e1697cae61125fad4ff37465
CeydaB4lci <ceyda.balci@sabanciuniv.edu>
2026-04-13 14:28:54 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,7 +4,7 @@ import '../utils/app_colors.dart';
 import '../utils/app_text_styles.dart';
 import '../utils/app_constants.dart';
 import '../widgets/bottom_nav_bar.dart';
-import '../widgets/app_bar.dart';
+import '../screens/side_screen.dart';
 
 
 class HomeScreen extends StatelessWidget {
