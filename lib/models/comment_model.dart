7584b608b3575ccb0740619b50124ba2d12da9f5
CeydaB4lci <ceyda.balci@sabanciuniv.edu>
2026-05-21 20:29:04 +0300

    Update comment_model.dart

--- lib/models/comment_model.dart
+++ lib/models/comment_model.dart
@@ -2,11 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CommentModel {
   final String id;
-  final String postId;
+  final String postId; 
   final String text;
   final String authorName;
   final String createdBy;
   final DateTime createdAt;
+  final double fontSize;
+  final int textColor;
+  final String fontFamily;
 
   CommentModel({
     required this.id,
@@ -15,6 +18,9 @@ class CommentModel {
     required this.authorName,
     required this.createdBy,
     required this.createdAt,
+    this.fontSize = 14.0,       
+    this.textColor = 0xFF000000, 
+    this.fontFamily = 'Poppins',  
   });
 
   factory CommentModel.fromFirestore(DocumentSnapshot doc) {
@@ -23,20 +29,25 @@ class CommentModel {
       id: doc.id,
       postId: data['postId'] ?? '',
       text: data['text'] ?? '',
-      authorName: data['authorName'] ?? '',
+      authorName: data['authorName'] ?? 'Anonymous',
       createdBy: data['createdBy'] ?? '',
       createdAt: (data['createdAt'] as Timestamp).toDate(),
+      fontSize: (data['fontSize'] as num?)?.toDouble() ?? 14.0,
+      textColor: data['textColor'] as int? ?? 0xFF000000,
+      fontFamily: data['fontFamily'] ?? 'Poppins',
     );
   }
 
   Map<String, dynamic> toFirestore() {
     return {
-      'id': id,
       'postId': postId,
       'text': text,
       'authorName': authorName,
       'createdBy': createdBy,
       'createdAt': Timestamp.fromDate(createdAt),
+      'fontSize': fontSize,
+      'textColor': textColor,
+      'fontFamily': fontFamily,
     };
   }
 }
