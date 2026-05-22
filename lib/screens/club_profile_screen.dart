65c19d0557272ae63bf12fb119a1d770489b6ee4
CeydaB4lci <ceyda.balci@sabanciuniv.edu>
2026-05-22 14:08:04 +0300

    Update club_profile_screen.dart

--- lib/screens/club_profile_screen.dart
+++ lib/screens/club_profile_screen.dart
@@ -6,8 +6,7 @@ import '../providers/auth_provider.dart';
 import '../providers/theme_provider.dart';
 import '../utils/app_colors.dart';
 import '../widgets/bottom_nav_bar.dart';
-import 'package:provider/provider.dart';
-import '../providers/auth_provider.dart';
+import '../screens/side_screen.dart';
 
 class ClubProfileScreen extends StatelessWidget {
   const ClubProfileScreen({super.key});
